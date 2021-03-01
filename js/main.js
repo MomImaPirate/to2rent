5c653faf1c97d2cde056633600bb0a30fadd9b78
Aleksandr_Berko <kum225new@gmail.com>
2021-03-01 20:15:48 +0200

    to2rent adaptive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+$('.slider').slick({
+    infinite:true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/slider-button-next.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/slider-button-prev.svg" alt="">',
+});
+
+});
